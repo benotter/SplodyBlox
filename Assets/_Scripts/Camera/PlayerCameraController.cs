090a196ab158a00b2a289db595308f47d3747699
Ben Otter <benotter@me.com>
2021-02-18 10:51:21 -0800

    Added Initial Director and Player Camera Control scripts

---
+++ Assets/_Scripts/Camera/PlayerCameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// As from the name, controls the player camera,
+// Also provides some handy 
+public class PlayerCameraController : MonoBehaviour
+{
+    // Max angles for the camera, otherwise you have to deal with sticky issues
+    // like gimbal lock and accidental rotations on unintended axis
+    public float maxUpAngle;
+    public float maxDownAngle;
+
+    // Just some formatting for the editor inspector
+    [Space(10)]
+
+    // Making a quick assumption that people will try both mouse drag
+    // and poking the arrow keys to rotate the camera, so need speed for both
+    public float clickdragSpeed;
+    public float keydownSpeed;
+
+    private Vector2 lastMousePos = Vector2.zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void UpdateMousePosition()
+    {
+        
+    }
+}
---
+++ Assets/_Scripts/Director.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * The Director runs the show, they generally control the scene and influence the 'actors'
+ * as well as keeps track of all the important player-relevant info
+ * like scoring, lifes, time/ammo remaining, time-elapsed, ect.
+ * Also keeps track of some programtic settings and developer options,
+ * Mostly a catch-all for scene top-level things
+*/
+public class Director : MonoBehaviour
+{
+    // The players score
+    public int playerScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
